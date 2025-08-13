249361f245d91676c0423d43083b51078ec15327
HellBats <vishalgoyal452003@gmail.com>
2025-08-13 11:25:09 +0000

    Initialized a window

---
+++ build/CMakeFiles/cpu_renderer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu_renderer.dir/src/main.cpp.o"
+  "CMakeFiles/cpu_renderer.dir/src/main.cpp.o.d"
+  "cpu_renderer"
+  "cpu_renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu_renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
